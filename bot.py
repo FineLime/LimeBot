c2e38a161e42fdf25b6fbd3a0390fd64cfad5639
FineLime <42850488+FineLime@users.noreply.github.com>
2018-12-03 19:30:39 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -687,7 +687,7 @@ async def on_message_delete(message):
     logschannel = get(message.server.channels, name="logs-spam")
     pic = False
     embed = discord.Embed(title="Deleted Message" , color=0x00ff00)
-    embed.add_field(name="@" + message.author.id, value=message.content, inline = False)
+    embed.add_field(name="@" + message.author.name + "#" + message.author.discriminator, value=message.content, inline = False)
     if len(message.attachments) > 0: 
         pic_ext = ["jpg", "jpeg", "gif", "png", "tiff", "bmp"]
         for ext in pic_ext:
