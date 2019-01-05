7c96d425615202e045648b3f0a35deffdb01017c
FineLime <42850488+FineLime@users.noreply.github.com>
2019-01-05 15:14:14 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -712,12 +712,6 @@ async def on_message(message):
             cooldown_lovetest.append(message.author.id)
             await asyncio.sleep(cooldown)
             cooldown_lovetest.remove(message.author.id)
-    
-    if msg.startswith(";test"): 
-        await bot.send_message(message.author, os.getenv('PEANUT'))
-        os.getenv('PEANUT') = 5
-        await bot.send_message(message.author, os.getenv('PEANUT'))
-    
         
            
 
