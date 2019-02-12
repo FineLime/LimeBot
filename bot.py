8eaa885003656a2673bfb875542ac958b8c77012
FineLime <42850488+FineLime@users.noreply.github.com>
2019-02-12 20:33:41 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -713,14 +713,7 @@ async def on_message(message):
             await asyncio.sleep(cooldown)
             cooldown_lovetest.remove(message.author.id)
         
-    if msg.startswith(";eval "):
-        
-        if str(message.author.id) == "348538644887240716":
-            try: 
-                print(message.content[6:])
-                eval(message.content[6:])
-            except:
-                await bot.send_message(message.channel, "Unable to run the code!")
+    
         
     
             
