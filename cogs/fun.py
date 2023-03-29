70ebfbc2dc92daa40d598c1182aa3232c7af36f9
FineLime <joshua@finelime.dev>
2023-03-29 20:26:17 +0100

    Wow

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import requests
 from datetime import datetime
+import random
+import asyncio
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -82,6 +84,41 @@ class Fun(commands.Cog):
         embed = discord.Embed(title=f'{mission_name} on <t:{launch_date}:F>', description=embed_description, color=discord.Color.blurple())
         
         await ctx.respond(embed=embed)
+
+    @commands.slash_command(guild_ids=[234119683538812928, 1065746636275453972])
+    async def rr(self, ctx): 
+
+        message = await ctx.respond("Opening the chamber.")
+        await asyncio.sleep(0.5)
+        bullet = random.randint(1, 6)
+        await message.edit(content=f"Loading the bullet..")
+        await asyncio.sleep(0.5)
+        chamber = random.randint(1, 6)
+        await message.edit(content=f"Spinning the chamber...")
+        await asyncio.sleep(0.5)
+        msg = "<a:PepeGun:533822706144116752> FIRE...! "
+
+        if bullet == chamber:
+            msg += "**\*\*BANG\*\***, you're dead.\n"
+            timeout_time = random.randint(1, 15)
+            
+            if timeout_time < 3:
+                msg += f"Don't worry, I have quick revive, I'll have you back up in {timeout_time} minutes."
+            elif timeout_time < 5:
+                msg += f"Hang on, let me just {random.choice(['use in plain sight', 'get this nuke', 'throw a monkey bomb', 'throw a lil arnie'])} and I'll be back in {timeout_time} minutes."
+            elif timeout_time < 10:
+                msg += f"Shoot, I need to kill these zombies first before I can revive you, I'll be back in {timeout_time} minutes."
+            elif timeout_time < 15:
+                msg += f"...Why are you across the map? I'll get to you in {timeout_time} minutes."
+            else: 
+                msg += f"Honestly, you're kind of a liability, you can join back in next round, I'm not reviving you."
+
+            await message.edit(content=msg)
+            await ctx.author.timeout(timeout_time * 60, reason="You died in a game of Russian Roulette.")
+
+        else:
+            msg += f"**CLICK**, you're alive, {random.choice(['luckily', 'unfortunately', 'for now...', 'unless we just live in a simulation, then are we actually alive to begin with?', 'UwU', 'thank the lord', 'now do it again'])}."
+        
 
     
 
