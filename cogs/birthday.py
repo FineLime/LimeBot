3506339fde6bc4cfed025e4a98bc311e8b37fb7b
unknown <joshua@finelime.dev>
2023-03-29 11:40:58 +0100

    setup function

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -2,8 +2,6 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
-async def check_birthdays
-
 class Birthday(commands.Cog):
 
     def __init__(self, bot):
