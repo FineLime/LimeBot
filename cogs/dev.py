d6249ad4ec501e4ce48d6af6128c0dfc881bc61a
FineLime <joshua@finelime.dev>
2023-04-11 20:32:56 +0100

    dev

---
+++ cogs/dev.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+
+def is_dev(ctx):
+    return str(ctx.author.id) in ["348538644887240716"]
+
+class Dev(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @is_dev()
+    @commands.slash_command(guild_ids=[234119683538812928, 1065746636275453972])
+    async def server_count(self, ctx):
+        await ctx.respond(f"Server Count: {len(self.bot.guilds)}")
+
+    @is_dev()
+    @commands.slash_command(guild_ids=[234119683538812928, 1065746636275453972])
+    async def eval(self, ctx, *, code):
+        code = code.strip("` ").split("python\n")[1]
+        try:
+            exec(code)
+        except Exception as e:
+            await ctx.respond(f"```py\n{e}```", ephemeral=True)
+            return
+        
+        await ctx.respond("Executed successfully", ephemeral=True)
+
+    @is_dev()
+    @commands.slash_command(guild_ids=[234119683538812928, 1065746636275453972])
+    async def aeval(self, ctx, *, code):
+        code = code.strip("` ").split("python\n")[1]
+        try:
+            await exec(code)
+        except Exception as e:
+            await ctx.respond(f"```py\n{e}```", ephemeral=True)
+            return
+        
+        await ctx.respond("Executed successfully", ephemeral=True)
+
+    @is_dev()
+    @commands.slash_command(guild_ids=[234119683538812928, 1065746636275453972])
+    async def return_aeval(self, ctx, *, code):
+        code = code.strip("` ").split("python\n")[1]
+        try:
+            result = await exec(code)
+        except Exception as e:
+            await ctx.respond(f"```py\n{e}```", ephemeral=True)
+            return
+        
+        await ctx.respond(f"{result}", ephemeral=True)
+        
+
+def setup(bot):
+    bot.add_cog(Dev(bot))
