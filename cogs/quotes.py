67a853ec675ed5e56ee7af4d7d2640750ad50be3
FineLime <joshua@finelime.dev>
2023-04-15 15:57:13 +0100

    quotes

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -33,6 +33,16 @@ class Quotes(commands.Cog):
         await self.bot.db.execute("DELETE FROM controlpanel_quotes WHERE guild_id = $1 AND quote_id = $2", ctx.guild.id, quote_id)
         await ctx.respond("Quote removed.")
 
+    @commands.slash_command(guild_ids=[234119683538812928, 1065746636275453972], description="Edit a quote")
+    @default_permissions(manage_messages=True)
+    async def editquote(self, ctx, quote_id: Option(int, "Quote ID"), message: Option(str, "What to quote?")):
+
+        #Make quote multiline
+        message = message.replace("\\n", "\n")
+
+        await self.bot.db.execute("UPDATE controlpanel_quotes SET quote = $1 WHERE guild_id = $2 AND quote_id = $3", message, ctx.guild.id, quote_id)
+        await ctx.respond("Quote edited.")
+
     @commands.slash_command(guild_ids=[234119683538812928, 1065746636275453972], description="Get a quote")
     async def quote(self, ctx, quote_id: Option(int, "Quote ID") = None):
 
@@ -48,6 +58,7 @@ class Quotes(commands.Cog):
 
         embed = discord.Embed(title=f"Quote #{quote_id}", description=data['quote'], color=discord.Color.blurple())
         await ctx.respond(embed=embed)
+
 
 
 def setup(bot):
